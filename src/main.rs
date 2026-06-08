65186bd5a200f31d428fa10953153d88e6310409
rezahsnz <rezahsnz@proton.me>
2026-06-08 17:21:55 +0330

    fix reserved mode

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,20 @@ async fn main() -> anyhow::Result<()> {
     let elf: Elf = fs::read("elf/subblock.bin")?.into();
     let stdin = SP1Stdin::from(&fs::read("elf/0.bin")?);
     let prover = ProverClient::builder()
-        .network()
+        .network_for(NetworkMode::Reserved)
         .rpc_url("http://gateway.internal:50061")
-        .hosted()
         .build()
         .await;
     let pk = prover
         .setup(elf)
         .await?;
+    println!("setup is complete.");
     let proof = prover
         .prove(&pk, stdin)
         .compressed()
         .await?;
+    println!("proof is ready!");
+    let _ = proof.save("./proof.bin");
 
     Ok(())
 }
