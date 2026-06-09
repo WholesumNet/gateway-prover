487aa42c4d137289bc5d1fdf8a0d0348971460d1
rezahsnz <rezahsnz@proton.me>
2026-06-09 17:52:02 +0330

    chore: blobs from sp1 v6.2.3

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use tokio;
 use sp1_sdk::{Elf, ProveRequest, Prover, ProverClient, SP1Stdin,
     network::NetworkMode,
+    SP1ProofWithPublicValues,
+    ProvingKey
 };
 
 use std::fs;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-
-    let elf: Elf = fs::read("elf/subblock.bin")?.into();
-    let stdin = SP1Stdin::from(&fs::read("elf/0.bin")?);
+    let elf: Elf = fs::read("blobs/subblock.bin")?.into();
+    let stdin = SP1Stdin::from(&fs::read("blobs/0.bin")?);
     let prover = ProverClient::builder()
         .network_for(NetworkMode::Reserved)
         .rpc_url("http://localhost:50061")
@@ -25,6 +26,24 @@ async fn main() -> anyhow::Result<()> {
         .await?;
     println!("proof is ready!");
     let _ = proof.save("./proof.bin");
+    // verify().await?;
 
+    Ok(())
+}
+
+pub async fn verify() -> anyhow::Result<()> {
+    println!("Initializing SP1.");
+    let cpu_client = ProverClient::builder()
+        .cpu()
+        .build()
+        .await;
+    let elf: Elf = fs::read("blobs/subblock.bin").unwrap().into();
+    let pk = cpu_client
+        .setup(elf)
+        .await?;        
+    let proof = SP1ProofWithPublicValues::load("blobs/proof.bin")?;
+    println!("verifying...");
+    cpu_client.verify(&proof, pk.verifying_key(), None)?;
+    println!("WOW, verified!");
     Ok(())
 }
